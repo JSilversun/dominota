36d9fa0b08c43c664aaa519d67873ad8f40ca78e
Yuleidy Valladares <yuleidyvalladares@gmail.com>
2018-02-24 10:27:26 -0400

    inicio de proyecto

---
+++ Dominota/src/dominota/Dominota.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominota;
+
+/**
+ *
+ * @author Yuleidy
+ */
+public class Dominota {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
